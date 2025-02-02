8a701deb178d89aa942d519c2f592148d404557e
AITBELLAouss <ouss.chute@gmail.com>
2025-02-02 18:41:32 +0000

    ai module

---
+++ main.py
@@ -0,0 +1,5 @@
+import os
+from crewai import Agent, Task, Crew, process
+
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["OPENAI_MODEL_NAME"] = os.getenv("OPENAI_MODEL_NAME")
